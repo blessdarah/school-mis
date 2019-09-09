57e16c0d76b96d4180e1360ba2d117088245d28c
blessdarah <blessdarahuba@gmail.com>
2019-09-09 06:58:00 +0100

    Reminder file and form created

--- resources/views/staff/reminders.blade.php
+++ resources/views/staff/reminders.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts/teacher')
 
 @section('content')
-<h3>Create a new reminder</h3>
+
+<div class="mt-5">
+  <form action="#" class="form-inline mx-auto">
+    <div class="form-group">
+      <div class="input-group mr-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Reminder</span>
+        </div>
+        <input type="text" class="form-control" placeholder="Add a reminder">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Remind me on</span>
+        </div>
+        <input type="date" class="form-control">
+      </div>
+    </div>
+    <input type="submit" value="Add new reminder" class="btn btn-success ml-3">
+  </form>
+</div>
+
 @endsection
