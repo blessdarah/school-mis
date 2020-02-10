fe5d4c2ac1da847abc04a1db922528ba3ba775c3
blessdarah <blessdarahuba@gmail.com>
2020-02-10 18:17:44 +0100

    link public storage directory and add font awesome cdn

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,6 +12,9 @@
         <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
         <link href="{{asset('css/app.css')}}" rel="stylesheet" />
 
+        {{-- font-awsome-cdn --}}
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
         {{-- Bootstrap cdn --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
